0b316cd7f93678de159677e4082b4f2842f596d8
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-12 18:21:10 -0400

    Update linear_search.py

--- algorithms/linear_search.py
+++ algorithms/linear_search.py
@@ -1,1 +1,9 @@
+from typing import List
 
+# 1. Search a list of ints for a particular integer. Return the index location, -1 if not found.
+
+def find_num(nums: List[int], target: int) -> int:
+  for i, num in enumerate(nums):
+      if num == target:
+          return i
+  return -1
