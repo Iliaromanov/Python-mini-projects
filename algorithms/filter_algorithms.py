b02c195d63c541ea2077f72f320d5e2a4b208c49
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-15 20:58:08 -0400

    Update filter_algorithms.py

--- algorithms/filter_algorithms.py
+++ algorithms/filter_algorithms.py
@@ -1,1 +1,10 @@
 
+# 1. Create a function that will take a list of numbers and return a new list of only the even numbers from the original list.
+
+def filter_evens(nums: List[int]) -> List[int]:
+    new_list = []
+
+    for num in nums:
+        if num % 2 == 0:
+            new_list.append(num)
+    return new_list
