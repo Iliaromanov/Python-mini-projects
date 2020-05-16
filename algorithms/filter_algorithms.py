001891902d715486e466aba0a4cc50ab9dbf4072
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-16 10:43:43 -0400

    Update filter_algorithms.py

--- algorithms/filter_algorithms.py
+++ algorithms/filter_algorithms.py
@@ -8,3 +8,20 @@ def filter_evens(nums: List[int]) -> List[int]:
         if num % 2 == 0:
             new_list.append(num)
     return new_list
+
+
+# 2. Create a function that will take a string and return a new string with all the vowels converted to uppercase.
+
+def cap_vowels(words: str) -> str:
+    vowels = "aeiou"
+    new_string = ""
+
+    for letter in words:
+        if letter in vowels:
+            uppercase = letter.upper()
+            new_string += uppercase
+        else:
+            new_string += letter
+
+    return new_string
+
