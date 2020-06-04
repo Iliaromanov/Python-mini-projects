117c1e11de122a2789ab4159e5d5ee3896e403b1
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-04 17:29:03 -0400

    Update binary_search.py

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,1 +1,20 @@
 
+from typing import List
+
+def binary_search(nums: List[int], target: int) -> int:
+    nums.sort()
+
+    left = 0
+    right = len(nums)
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        result = nums[mid]
+
+        if result == target:
+            return mid
+        elif result > target:
+            right = mid
+        elif result < target:
+            left = mid
