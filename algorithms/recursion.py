7995b1b59d8be8a834b34bcdaba7b3f91b171fe2
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-23 17:39:05 -0400

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -1,4 +1,3 @@
-
 def bunnyEars(n: int) -> int:
     """We have a number of bunnies and each bunny has two big
     floppy ears. We want to compute the total number of ears
@@ -16,7 +15,17 @@ def bunnyEars(n: int) -> int:
 
 
 def factorial(n: int) -> int:
+    """
+    Given n of 1 or more, return the factorial of n, 
+    which is n * (n-1) * (n-2) ... 1. 
+    Compute the result recursively (without loops).
+    factorial(1) → 1
+    factorial(2) → 2
+    factorial(3) → 6
+    """
     if n == 1:
         return 1
 
-    return n * factorial(n-1)
+    return n * factorial(n - 1)
+
+
