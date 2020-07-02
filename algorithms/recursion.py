814787fbbf5377542ab1b39757ff59a47cc11fea
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-07-01 21:25:07 -0400

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -66,3 +66,17 @@ def bunnyEars2(n: int) -> int:
         return 3 + bunnyEars2(n-1)
     else:
         return 2 + bunnyEars2(n-1)
+    
+    
+def triangle(n: int) -> int:
+    """"
+    We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+    triangle(0) → 0
+    triangle(1) → 1
+    triangle(2) → 3
+    """  
+
+    if n == 0:
+        return 0
+    
+    return n + triangle(n-1)
