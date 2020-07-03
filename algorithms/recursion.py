c518162460a092ab249f8b1ee6720207298e50fe
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-07-03 14:10:16 -0400

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -80,3 +80,17 @@ def triangle(n: int) -> int:
         return 0
     
     return n + triangle(n-1)
+
+
+def sumDigits(n: int) -> int:
+    """
+    Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    sumDigits(126) → 9
+    sumDigits(49) → 13
+    sumDigits(12) → 3
+    """
+
+    if n < 10:
+        return n
+
+    return n % 10 + sumDigits(n // 10)
