46a9fcdb3fa1f3a3c123c0235223692daad9aaa9
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-07-05 23:55:32 -0400

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -94,3 +94,25 @@ def sumDigits(n: int) -> int:
         return n
 
     return n % 10 + sumDigits(n // 10)
+
+
+def count7(n: int) -> int:
+    """
+    Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    count7(717) → 2
+    count7(7) → 1
+    count7(123) → 0
+    """
+
+    if n < 10:
+        if n == 7:
+            return 1
+        else:
+            return 0    
+
+    num = n % 10
+
+    if num == 7:
+        return 1 + count7(n // 10)
+    else:
+        return count7(n // 10)
