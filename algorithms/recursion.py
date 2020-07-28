a17bff24bf6d322ff1946ffe3bc26f2c6d0bcd1d
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-07-27 22:57:13 -0400

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -256,3 +256,19 @@ def noX(string: str) -> str:
         return noX(string[1:])
     else:
         return string[0] + noX(string[1:])
+def array6(nums: List[int], index: int) -> bool:
+    """
+    Given an array of ints, compute recursively if the array contains a 6. We'll use the convention of considering only the part of the array that begins at the given index. In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+    array6([1, 6, 4], 0) → true
+    array6([1, 4], 0) → false
+    array6([6], 0) → true
+    """
+
+    if len(nums) == 0:
+            return False
+    
+    if nums[0] == 6:
+        return True
+    else:
+        return array6(nums[1:], index + 1)
+    
