6dde7b72b02c7764002dd491727549e12e831ab2
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-08-09 14:59:23 -0400

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -292,4 +292,19 @@ def array6(nums: List[int], index: int) -> bool:
         return 1 + array11(nums, i+1)
     else:
         return 0 + array11(nums, i+1)
+    
+    
+def array220(nums: List[int], index: int) -> bool:
+    """
+    Given an array of ints, compute recursively if the array contains somewhere a value followed in the array by that value times 10. We'll use the convention of considering only the part of the array that begins at the given index. In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+    array220([1, 2, 20], 0) → true
+    array220([3, 30], 0) → true
+    array220([3], 0) → false            
+    """
+    if index >= len(nums) - 1:
+        return False
 
+    if nums[index]*10 == nums[index+1]:
+        return True
+    
+    return array220(nums, index+1)
