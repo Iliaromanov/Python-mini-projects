e133c811921b70d79effb74daa59540edf7f34a0
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-09-06 11:25:55 -0400

    Create readability_calculator.py

---
+++ cs50/readability_calculator.py
@@ -0,0 +1,14 @@
+
+text = input("Text: ")
+
+def count_letters(text: str) -> int:
+    pass
+
+def count_words(text: str) -> int:
+    pass
+
+def count_sentences(text: str) -> int:
+    pass
+
+def calculate_grade(letters: int, words: int, sentences: int) -> int:
+    pass
