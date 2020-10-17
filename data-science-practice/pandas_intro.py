e117f92bf171b46afc0a8c0176d219ba148a72ed
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-10-16 20:11:57 -0400

    Update pandas_intro.py

--- data-science-practice/pandas_intro.py
+++ data-science-practice/pandas_intro.py
@@ -2,5 +2,7 @@
 import pandas as pd
 
 google_play = "/Users/iliarom.BASE.000/Desktop/Data-Science/google-playstore-data.csv"
-df = pd.read_csv(google_play)
+
+# read_csv can be used to read .csv files
+df = pd.read_csv(google_play) 
 print(df)
