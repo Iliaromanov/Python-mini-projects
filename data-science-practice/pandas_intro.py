d27ed71136a9c7167e91b5861605fede22249738
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-10-18 09:00:21 -0400

    Update pandas_intro.py

--- data-science-practice/pandas_intro.py
+++ data-science-practice/pandas_intro.py
@@ -4,5 +4,16 @@ import pandas as pd
 google_play = "/Users/iliarom.BASE.000/Desktop/Data-Science/google-playstore-data.csv"
 
 # read_csv can be used to read .csv files
-df = pd.read_csv(google_play) 
-print(df)
+df = pd.read_csv(google_play) # df stands for data frame
+
+print(df) # prints out the data frame in data table format
+print(len(df)) # prints the amount of rows in df (10841)
+print(df.columns) # prints column headers in array format
+
+# in pandas 'object' means string
+
+# .info method provides useful info on data frame
+print(df.info())
+
+# .dtype provides a data table describing the dtype of each column
+print(df.dtypes)
